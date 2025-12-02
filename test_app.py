50cb1974880430cc9b67f31f23c29b7edb3d611e
Djoser SIMEU <97344225+DjoserKhemSimeu@users.noreply.github.com>
2025-12-02 10:05:01 +0100

    Remove test for current bug in addition function
    Removed test for NameError due to undefined variable.

--- test_app.py
+++ test_app.py
@@ -8,10 +8,3 @@ def test_addition_correct_behavior():
     """
     assert app.addition(2, 3) == 5
 
-def test_addition_raises_error_with_current_bug():
-    """
-    Vérifie qu'avec le bug actuel (utilisation de 'c' non défini),
-    la fonction lève bien une NameError.
-    """
-    with pytest.raises(NameError):
-        app.addition(1, 1)
