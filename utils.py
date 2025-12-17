13565de021f16585ef816a2e3dc194866673a417
root <root@hydra-1.lyon.grid5000.fr>
2025-12-18 00:27:51 +0100

    Fix: Handle negative input in calculate_result

---
+++ utils.py
@@ -0,0 +1,8 @@
+# utils.py
+
+def calculate_result(x):
+    """Calculates a result based on the input x."""
+    if x < 0:
+        return 0  # Handle negative input
+    else:
+        return x * 2
